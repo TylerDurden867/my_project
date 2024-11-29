7eac4e17f31110f3573e27d9bfda6e0f22322eee
TylerDurden867 . <aftyka.milosz@wp.pl>
2024-11-29 11:06:37 +0000

    zadanie

--- src/my_project_backend/src/lib.rs
+++ src/my_project_backend/src/lib.rs
@@ -1,4 +1,24 @@
+use std::cell::RefCell;
+
+thread_local! {
+    static CHAT: RefCell<Vec<String>> = RefCell::new(Vec::new());
+}
+
+#[ic_cdk::update]
+fn save_msg(msg: String) {
+    CHAT.with(|msgs| msgs.borrow_mut().push(msg));
+}
+
+#[ic_cdk::query]
+fn get_chat() -> Vec<String> {
+    CHAT.with(|msgs| msgs.borrow().clone())
+}
+
 #[ic_cdk::query]
 fn greet(name: String) -> String {
     format!("Hello, {}!", name)
 }
+
+
+
+a
